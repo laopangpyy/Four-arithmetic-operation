f102ddd47bd17e20a8d5505e30d403a827a3e813
laopangpyy <40331961+laopangpyy@users.noreply.github.com>
2019-01-20 22:10:28 +0800

    Update Program.cs

--- All Codes/Program.cs
+++ All Codes/Program.cs
@@ -5,10 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
-/// <summary>
-/// mode=1 基础，mode=2 带分数，mode=3，带乘方。
-/// 何神的bug没有改
-/// </summary>
+
 namespace CalculatorGame
 {
     static class Program
